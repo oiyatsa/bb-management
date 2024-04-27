640c8aee3cabeef4671d913d75b571a95d1d731e
oiyatsa <s1216ayu@gmail.com>
2024-04-27 15:07:54 +0900

    categoriesテーブル作成・データ挿入、リレーション記述

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Food extends Model
+class Category extends Model
 {
     use HasFactory;
-    protected $table = 'foods';
 }
 
-public function category()
+public function foods()   
 {
-    return $this->belongsTo(Category::class);
+    return $this->hasMany(Food::class);  
 }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'category_name' => '冷蔵庫',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'category_name' => '冷凍庫',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'category_name' => '野菜室',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'category_name' => 'その他',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        
+        
+
+        
+    }
+}
